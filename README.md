a038eff4ff866f1161cbdb966e46c2c4c74fe215
ranxx <ran.star@foxmail.com>
2022-08-18 19:23:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# docker-compose
+
+docker-compose 集合, 请移步 [docker-compose,yaml](./docker-compose.yaml)
---
+++ mysql/init.sql
@@ -0,0 +1,5 @@
+Create Database If Not Exists lucky Character Set utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+use lucky;
+
+GRANT All privileges ON *.* TO 'root'@'%';
